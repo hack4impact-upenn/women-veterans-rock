b2a7d292aa7256784eaecc3c647788e6d2539af3
Yoni Nachmany <nachmany@seas.upenn.edu>
2015-10-27 16:19:19 -0400

    After copying Address model to location.py in rani, deleted from resource.py

--- app/models/resource.py
+++ app/models/resource.py
@@ -13,17 +13,3 @@ class Resource(db.Model):
 
     def __repr__(self):
         return '<Resource \'%s\'>' % self.name
-
-
-class Address(db.Model):
-    __tablename__ = 'addresses'
-    id = db.Column(db.Integer, primary_key=True)
-    # http://pe.usps.com/text/pub28/28c2_001.htm
-    recipient_line = db.Column(db.Text)         # ABC MOVERS
-    delivery_address_line = db.Column(db.Text)  # 1500 E MAIN AVE STE 201
-    city = db.Column(db.Text)
-    state = db.Column(db.String(2))
-    zip_code = db.relationship('ZIPCode', backref='address', lazy='dynamic')
-
-    def __repr__(self):
-        return '<Address \'%s\'>' % self.receipt_line
