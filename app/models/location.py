4c7e603fbaf994a79f1647afc9003a5755b36fb9
Yoni Nachmany <nachmany@seas.upenn.edu>
2015-10-27 16:17:56 -0400

    Copied Address model into location.py

--- app/models/location.py
+++ app/models/location.py
@@ -10,3 +10,17 @@ class ZIPCode(db.Model):
 
     def __repr__(self):
         return '<ZIPCode \' %s \'>' % self.zipcode
+
+
+class Address(db.Model):
+    __tablename__ = 'addresses'
+    id = db.Column(db.Integer, primary_key=True)
+    # http://pe.usps.com/text/pub28/28c2_001.htm
+    recipient_line = db.Column(db.Text)         # ABC MOVERS
+    delivery_address_line = db.Column(db.Text)  # 1500 E MAIN AVE STE 201
+    city = db.Column(db.Text)
+    state = db.Column(db.String(2))
+    zip_code = db.relationship('ZIPCode', backref='address', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Address \'%s\'>' % self.receipt_line
