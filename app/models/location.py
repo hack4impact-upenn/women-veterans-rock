9d0c4247bbb3e344b00d8976d8bba9f0fcfd4465
Yoni Nachmany <nachmany@seas.upenn.edu>
2015-10-28 11:09:20 -0400

    Added USPS Addressing Standards comment, changed zip_code field in Address to zip_code_id

--- app/models/location.py
+++ app/models/location.py
@@ -15,12 +15,12 @@ class ZIPCode(db.Model):
 class Address(db.Model):
     __tablename__ = 'addresses'
     id = db.Column(db.Integer, primary_key=True)
-    # http://pe.usps.com/text/pub28/28c2_001.htm
+    # USPS Addressing Standards: http://pe.usps.com/text/pub28/28c2_001.htm
     recipient_line = db.Column(db.Text)         # ABC MOVERS
     delivery_address_line = db.Column(db.Text)  # 1500 E MAIN AVE STE 201
     city = db.Column(db.Text)
     state = db.Column(db.String(2))
-    zip_code = db.relationship('ZIPCode', backref='address', lazy='dynamic')
+    zip_code_id = db.Column(db.Integer, db.ForeignKey('zip_code.id'))
 
     def __repr__(self):
         return '<Address \'%s\'>' % self.receipt_line
