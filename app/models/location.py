1f89001fc595c65af10786750818d5d30e29c406
riyer15 <rani.iyer15@gmail.com>
2015-10-28 15:35:51 -0400

    made zipcodes strings, fixed one-to-many relationships, and fixed naming

--- app/models/location.py
+++ app/models/location.py
@@ -2,14 +2,14 @@ from .. import db
 
 
 class ZIPCode(db.Model):
-    __tablename__ = 'ZIPCodes'
+    __tablename__ = 'zip_codes'
     id = db.Column(db.Integer, primary_key=True)
-    zipcode = db.Column(db.Integer, unique=True)
+    zip_code = db.Column(db.String, unique=True)
     users = db.relationship('ZIPCode', backref='user', lazy='dynamic')
     resources = db.relationship('ZIPCode', backref='resource', lazy='dynamic')
 
     def __repr__(self):
-        return '<ZIPCode \' %s \'>' % self.zipcode
+        return '<ZIPCode \' %s \'>' % self.zip_code
 
 
 class Address(db.Model):
