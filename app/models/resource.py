2730a0bfb980b04ccc6bbadd00bee81d999ea0e7
Yoni Nachmany <nachmany@seas.upenn.edu>
2015-11-02 10:09:46 -0500

    deleted two lines, so pr can stand on its own

--- app/models/resource.py
+++ app/models/resource.py
@@ -5,11 +5,8 @@ class Resource(db.Model):
     __tablename__ = 'resources'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
-    address = db.relationship('Address', backref='resource', lazy='joined',
-                              uselist=False)
     description = db.Column(db.Text)
     website = db.Column(db.Text)
-    reviews = db.relationship('Review', backref='resource', lazy='dynamic')
 
     def __repr__(self):
         return '<Resource \'%s\'>' % self.name
