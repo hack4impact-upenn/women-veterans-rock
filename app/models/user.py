05ab63d1712f81f7fd4ecda78024c83100e7d60d
Yoni Nachmany <nachmany@seas.upenn.edu>
2015-11-02 17:07:21 -0500

    Resources imported and relationship added in User

--- app/models/user.py
+++ app/models/user.py
@@ -4,6 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, \
     BadSignature, SignatureExpired
 from .. import db, login_manager
+from resource import Resource, ResourceReview  # noqa
 
 
 class Permission:
@@ -53,6 +54,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    resources = db.relationship('Resource', backref='user', lazy='dynamic')
     resource_reviews = db.relationship('ResourceReview', backref='user',
                                        lazy='dynamic')
 
