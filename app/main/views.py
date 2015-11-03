933049896c46de6981d65941a4726ab8c43d8d56
riyer15 <rani.iyer15@gmail.com>
2015-11-03 15:32:53 -0500

    adding in longitude/latitude columns

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,14 @@
-from flask import render_template
+from flask import request, render_template
 from . import main
 
 
 @main.route('/')
 def index():
     return render_template('main/index.html')
+
+
+@main.route('/map', method='[POST]')
+def map():
+    content = request.get_json(force=True)
+    coors = content['zip_codes']
+    print coors
