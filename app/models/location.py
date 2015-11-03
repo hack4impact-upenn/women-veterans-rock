0d3c086f12875092df960446fdb44a9182ae96a4
riyer15 <rani.iyer15@gmail.com>
2015-11-03 00:02:05 -0500

    changed zipcode length back to 5 and fixed relationships b/t addresses, zipcodes

--- app/models/location.py
+++ app/models/location.py
@@ -4,12 +4,12 @@ from .. import db
 class Zipcode(db.Model):
     __tablename__ = 'zip_codes'
     id = db.Column(db.Integer, primary_key=True)
-    zip_code = db.Column(db.String(10), unique=True)
+    zip_code = db.Column(db.String(5), unique=True)
     users = db.relationship('User', backref='zip_code', lazy='dynamic')
-    resources = db.relationship('Resource', backref='zip_code', lazy='dynamic')
+    addresses = db.relationship('Address', backref='zip_code', lazy='dynamic')
 
     def __repr__(self):
-        return '<ZIPCode \' %s \'>' % self.zip_code
+        return '<ZIPCode \'%s \'>' % self.zip_code
 
 
 class Address(db.Model):
@@ -20,7 +20,9 @@ class Address(db.Model):
     delivery_address_line = db.Column(db.Text)  # 1500 E MAIN AVE STE 201
     city = db.Column(db.Text)
     state = db.Column(db.String(2))
-    zip_code_id = db.Column(db.Integer, db.ForeignKey('zip_code.id'))
+    zip_code_id = db.Column(db.Integer, db.ForeignKey('zip_codes.id'))
+    resources = db.relationship('Resource',
+                                backref='addresses', lazy='dynamic')
 
     def __repr__(self):
         return '<Address \'%s\'>' % self.receipt_line
