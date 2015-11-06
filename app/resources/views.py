e4c71261de87a06f1d2de6388a7fbc82471931e4
Yoni Nachmany <nachmany@seas.upenn.edu>
2015-11-06 15:20:14 -0500

    Check if zip code, address exist or load, in order to use address id in resourc

--- app/resources/views.py
+++ app/resources/views.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for, flash  # request
 from flask.ext.login import current_user
 from . import resources
 from .. import db
-from ..models import Resource
+from ..models import Resource, ZIPCode, Address
 from .forms import ResourceForm
 
 
@@ -15,15 +15,28 @@ def index():
 def add():
     form = ResourceForm()
     if form.validate_on_submit():
-        print current_user.get_id()
-        # zip_code = ZIPCode.filter_by(zip_code=form.zip_code.data).first()
-        # address = Address.filter_by()
+        # based on the form's zip code, load or create ZIPCode
+        zip_code = ZIPCode.filter_by(zip_code=form.zip_code.data).first() or \
+            ZIPCode(zip_code=form.zip_code.data)
+        # based on the form's address and zip code id, load or create Address
+        address = Address.filter_by(name=form.name.data,
+                                    street_address=form.street_address.data,
+                                    city=form.street_address.data,
+                                    state=form.state.data,
+                                    zip_code_id=zip_code.id,).first() or \
+            Address(name=form.name.data,
+                    street_address=form.street_address.data,
+                    city=form.city.data,
+                    state=form.state.data,
+                    zip_code_id=zip_code.id)
+        # based on form and address id, create a new resource
         resource = Resource(name=form.name.data,
                             description=form.description.data,
-                            website=form.website.data)
-        # address_id=, # user_id=current_user.get_id())
+                            website=form.website.data,
+                            address_id=address.id,
+                            user_id=int(current_user.get_id()))
         db.session.add(resource)
         db.session.commit()
-        flash('Resource has been added')
-        return redirect(url_for('resources.index'))
+        flash('Resource has been added.')
+        return redirect(url_for('resources/index'))
     return render_template('resources/add.html', form=form)
