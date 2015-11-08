b0cc9061f532c06ff618f8a06635d9b490bf6042
veronicawharton <veronica.wharton@yahoo.com>
2015-11-08 15:31:08 -0500

    Added association tables

--- app/models/attribute.py
+++ app/models/attribute.py
@@ -1,13 +1,27 @@
 from .. import db
 
+users_association_table = db.Table(
+    'users_association', db.Model.metadata,
+    db.Column('tags_id', db.Integer, db.ForeignKey('tags.id')),
+    db.Column('users_id', db.Integer, db.ForeignKey('users.id'))
+)
+
+resources_association_table = db.Table(
+    'resources_association', db.Model.metadata,
+    db.Column('tags_id', db.Integer, db.ForeignKey('tags.id')),
+    db.Column('resources_id', db.Integer, db.ForeignKey('resources.id'))
+)
+
 
 class Tag(db.Model):
-    __tablename__ = 'tag'
+    __tablename__ = 'tags'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), unique=True)
-    users = db.relationship('User', backref='tag', lazy='dynamic')
-    resources = db.relationship('Resource')
-    addresses = db.relationship('Address', backref='zip_code', lazy='dynamic')
+    users = db.relationship('User', secondary=users_association_table,
+                            backref='tags', lazy='dynamic')
+    resources = db.relationship('Resource',
+                                secondary=resources_association_table,
+                                backref='tags', lazy='dynamic')
 
     def __repr__(self):
-        return '<ZIPCode \'%s\'>' % self.zip_code
+        return '<Tag \'%s\'>' % self.name
