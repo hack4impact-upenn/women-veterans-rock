6c3d0f35da737854b3c8cab98ed83ca770d68b00
veronicawharton <veronica.wharton@yahoo.com>
2015-11-12 03:09:01 -0500

    More renaming

--- app/models/attribute.py
+++ app/models/attribute.py
@@ -1,14 +1,15 @@
 from .. import db
 
-user_tag_associations = db.Table(
-    'users_association', db.Model.metadata,
+user_tag_associations_table = db.Table(
+    'user_tag_associations', db.Model.metadata,
     db.Column('tag_id', db.Integer, db.ForeignKey('tags.id')),
     db.Column('user_id', db.Integer, db.ForeignKey('users.id'))
 )
 
-resource_tag_associations = db.Table(
-    'resources_association', db.Model.metadata,
-    db.Column('tag_id', db.Integer, db.ForeignKey('tags.id')),
+resource_tag_associations_table = db.Table(
+    'resource_tag_associations', db.Model.metadata,
+    db.Column('resource_tag_associations', db.Integer,
+              db.ForeignKey('tags.id')),
     db.Column('resource_id', db.Integer, db.ForeignKey('resources.id'))
 )
 
@@ -17,10 +18,10 @@ class Tag(db.Model):
     __tablename__ = 'tags'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), unique=True)
-    users = db.relationship('User', secondary=user_tag_associations,
+    users = db.relationship('User', secondary=user_tag_associations_table,
                             backref='tags', lazy='dynamic')
     resources = db.relationship('Resource',
-                                secondary=resource_tag_associations,
+                                secondary=resource_tag_associations_table,
                                 backref='tags', lazy='dynamic')
 
     def __repr__(self):
