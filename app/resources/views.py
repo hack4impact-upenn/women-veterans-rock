eab9aad7bdc3486bfee38b143cdf66c41f58cf45
Yoni Nachmany <nachmany@seas.upenn.edu>
2015-11-13 09:56:09 -0500

    cast int to string

--- app/resources/views.py
+++ app/resources/views.py
@@ -19,17 +19,17 @@ def add():
         # based on form's zip code, load or create ZIPCode and add to db
         zip_code = ZIPCode.create_zip_code(form.postal_code.data)
         # based on form's address, zip's id, load or create Address, add to db
-        street_num_and_route = form.street_number.data + ' ' + form.route.data
+        street_num_route = str(form.street_number.data) + ' ' + form.route.data
         # should create helper method in models/location.py
         address = Address.query.filter_by(
             name=form.name.data,
-            street_address=street_num_and_route,
+            street_address=street_num_route,
             city=form.locality.data,
             state=form.administrative_area_level_1.data,
             zip_code_id=zip_code.id).first()
         if address is None:
             address = Address(name=form.name.data,
-                              street_address=street_num_and_route,
+                              street_address=street_num_route,
                               city=form.locality.data,
                               state=form.administrative_area_level_1.data,
                               zip_code_id=zip_code.id)
@@ -44,6 +44,7 @@ def add():
                             user_id=int(current_user.get_id()))
         db.session.add(resource)
         db.session.commit()
+        print resource.id
         return redirect(url_for('resources.index'))
     return render_template('resources/add.html', form=form)
 
