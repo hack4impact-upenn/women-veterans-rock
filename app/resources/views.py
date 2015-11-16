229d53b83954fc90819a1996985da7d5e8194fd2
Yoni Nachmany <nachmany@seas.upenn.edu>
2015-11-16 14:01:55 -0500

    Fixed comment style or added newlines

--- app/resources/views.py
+++ app/resources/views.py
@@ -16,11 +16,11 @@ def index():
 def add():
     form = ResourceForm()
     if form.validate_on_submit():
-        # based on form's zip code, load or create ZIPCode and add to db
+        # Based on form's zip code, load or create ZIPCode and add to db.
         zip_code = ZIPCode.create_zip_code(form.postal_code.data)
-        # based on form's address, zip's id, load or create Address, add to db
+        # Based on form's address, zip's id, load or create Address, add to db.
         street_num_route = str(form.street_number.data) + ' ' + form.route.data
-        # should create helper method in models/location.py
+        # Should create helper method in models/location.py.
         address = Address.query.filter_by(
             name=form.name.data,
             street_address=street_num_route,
@@ -35,8 +35,8 @@ def add():
                               zip_code_id=zip_code.id)
             db.session.add(address)
             db.session.commit()
-        # based on form and address id, create a new resource
-        # should create helper method in models/resource.py
+        # Based on form and address id, create a new resource.
+        # Should create helper method in models/resource.py.
         resource = Resource(name=form.name.data,
                             description=form.description.data,
                             website=form.website.data,
@@ -51,7 +51,7 @@ def add():
 
 @resources.route('/resource/<int:resource_id>')
 def show_resource(resource_id):
-    # show the resource with the given id, the id is an integer
+    # Show the resource with the given id, the id is an integer.
     resource = Resource.query.filter_by(id=resource_id).first()
     address = Address.query.filter_by(id=resource.address_id).first()
     user = User.query.filter_by(id=resource.user_id).first()
