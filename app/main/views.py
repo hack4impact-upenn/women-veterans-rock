1557c80b87c506dcb0484807da41cc0ca5e9dd1e
Elizabeth Hamp <ehamp@seas.upenn.edu>
2015-11-22 10:40:15 -0500

    added resource search

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from flask import render_template
 from flask import Response
 from . import main
 import json
-from ..models import User
+from ..models import User, Resource
 from flask.ext.login import login_required
 
 
@@ -31,6 +31,24 @@ def search_query(query):
     data = dict()
     data['results'] = [{'title': u.full_name(),
                         'url': '/account/profile/' + str(u.id)} for u in users]
+    json_data = json.dumps(data)
+    return Response(response=json_data, status=200,
+                    mimetype='application/json')
+
+
+@main.route('/resource_search')
+def search_resources():
+    return render_template('main/resource_search.html')
+
+
+@main.route('/resource_search/<query>')
+def search_query_resources(query):
+    looking_for = '%'+query+'%'
+    resources = Resource.query.filter(Resource.name.ilike(looking_for))\
+        .order_by(Resource.name).all()
+    data = dict()
+    data['results'] = [{'title': r.name,
+                        'url': '/resource/' + str(r.id)} for r in resources]
     json_data = json.dumps(data)
     return Response(response=json_data, status=200,
                     mimetype='application/json')
