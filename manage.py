ffb11ff4b70d39d0c3e3580197cc3bac5b44eda2
Anosha Minai <anoshaminai@gmail.com>
2015-12-06 15:53:59 -0500

    Populate dropdown with values from db

--- manage.py
+++ manage.py
@@ -20,7 +20,7 @@ migrate = Migrate(app, db)
 
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role, ZIPCode=ZIPCode,
-                Address=Address)
+                Address=Address, DonorLevel=DonorLevel)
 
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
